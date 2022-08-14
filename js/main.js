1d788fced8c150afa23384817f4aa7f8ea63bb06
Mónika <gergye.monika@gmail.com>
2022-08-14 13:09:54 +0200

    Második kisfeladat javascript első hét

--- js/main.js
+++ js/main.js
@@ -4,3 +4,19 @@
 - A függvény neve `generateList` legyen!
 - A HTML-template egy lista, amelynek a listaelemei a kapott tömb értékeit tartalmazzák.
 */
+const content = ['első', 'második', 'harmadik'];
+const content2 = ['első', 'második', 'harmadik','negyedik'];
+
+const generateList=function(tomb)
+{
+    let eredmeny="<ul> ";
+
+    for(let i=0;i<tomb.length;i=i+1)
+    {
+        eredmeny=eredmeny+"<li>"+tomb[i]+"</li> " 
+    }
+    return eredmeny+"</ul>"
+}
+
+console.log(generateList(content))
+console.log(generateList(content2))
